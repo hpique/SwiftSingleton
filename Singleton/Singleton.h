7b7b6cd89221b459047ddcc93e4eabc9f4729842
Hermes Pique <hermespique@gmail.com>
2014-07-21 23:43:56 +0200

    Update project for Beta 4

--- Singleton/Singleton.h
+++ Singleton/Singleton.h
@@ -2,7 +2,7 @@
 //  Singleton.h
 //  Singleton
 //
-//  Created by Hermes Pique on 6/9/14.
+//  Created by Hermes Pique on 7/21/14.
 //  Copyright (c) 2014 Hermes Pique. All rights reserved.
 //
 
@@ -15,3 +15,5 @@ FOUNDATION_EXPORT double SingletonVersionNumber;
 FOUNDATION_EXPORT const unsigned char SingletonVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Singleton/PublicHeader.h>
+
+
